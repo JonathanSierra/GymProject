bad69fa2e3884163e01425bc338fa5fe15a8a70a
Jonathan Sierra <jonathansie42@gmail.com>
2025-09-13 17:37:29 -0500

    Primer commit

---
+++ src/gymproject/Users.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gymproject;
+
+/**
+ *
+ * @author jonat
+ */
+public class Users {
+    
+    private int idUser;
+    private String user;
+    private String pass;
+    private int idRole;
+
+    public Users(int idUser, String user, String pass, int idRole) {
+        this.idUser = idUser;
+        this.user = user;
+        this.pass = pass;
+        this.idRole = idRole;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public int getIdRole() {
+        return idRole;
+    }
+
+    
+    
+    
+    
+}
